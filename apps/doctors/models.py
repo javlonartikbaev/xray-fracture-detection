e28c1b8b75f6f5c2246e717d33f833bd9eb01e16
Artikbaev Javlon <artjavlon@gmail.com>
2026-02-09 22:21:42 +0500

    add model doctors

---
+++ apps/doctors/models.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Date
+
+from settings.base import Base
+
+
+class Doctors(Base):
+    first_name: Mapped[str] = mapped_column(String(100))
+    last_name: Mapped[str] = mapped_column(String(100))
+    phone_number: Mapped[str] = mapped_column(String(16))
+    hashed_password: Mapped[str] = mapped_column(String())
+    date_of_birth: Mapped[date] = mapped_column(Date())
+    profile_img: Mapped[str | None] = mapped_column(String(255), nullable=True)
