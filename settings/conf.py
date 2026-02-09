f7d8e35727f5577ba341b7770497f50f1201e4b3
Artikbaev Javlon <artjavlon@gmail.com>
2026-02-09 21:56:01 +0500

    settings project

---
+++ settings/conf.py
@@ -0,0 +1,19 @@
+from pydantic import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+
+class Settings(BaseSettings):
+    DB_NAME: str
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PW: str
+
+    class Config:
+        env_file = ".env"
+
+
+settings = Settings()
---
+++ settings/db.py
@@ -0,0 +1,23 @@
+from typing import Annotated
+
+from fastapi import Depends
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session, declarative_base
+from .conf import settings
+
+DB_URL = f'postgresql://{settings.DB_USER}:{settings.DB_PW}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}'
+
+engine = create_engine(DB_URL)
+session_factory = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+Base = declarative_base()
+
+
+def get_db():
+    db = session_factory()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
