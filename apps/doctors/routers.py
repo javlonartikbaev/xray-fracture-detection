b3408fe720cf3f1ea783e4730926d1d8b6940e8c
Artikbaev Javlon <artjavlon@gmail.com>
2026-03-02 17:00:25 +0500

    add hash password

--- apps/doctors/routers.py
+++ apps/doctors/routers.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import Session
 from starlette import status
 
 from apps.doctors.schemas import DoctorCreate, DoctorRead
-from apps.doctors.services import create_doctor, get_doctors
+from apps.doctors.services import create_doctor_service, get_doctors_service, get_doctor_service
 from apps.registration.schemas import UserRead
 from settings.db import get_db
 
@@ -18,14 +18,22 @@ router = APIRouter(prefix='/doctors', tags=['Doctors'])
 @router.post('/register', status_code=status.HTTP_201_CREATED, response_model=DoctorRead)
 async def register_doctor(doctor: DoctorCreate, db: AsyncSession = Depends(get_db)):
     try:
-        doctor = await create_doctor(doctor, db)
-    except:
-        raise HTTPException(status_code=400, detail='Doctor with this phone number already exists')
+        doctor = await create_doctor_service(doctor, db)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
     return doctor
 
 
 @router.get('/list', status_code=status.HTTP_200_OK, response_model=List[DoctorRead])
 async def list_doctors(db: AsyncSession = Depends(get_db), skip: int = 0, limit: int = 10):
-    result = await get_doctors(db, skip, limit)
+    result = await get_doctors_service(db, skip, limit)
     return result
 
+
+@router.get('/{doctor_id}', status_code=status.HTTP_200_OK, response_model=DoctorRead)
+async def get_doctor(doctor_id: int, db: AsyncSession = Depends(get_db)):
+    try:
+        doctor = await get_doctor_service(db, doctor_id)
+    except:
+        raise HTTPException(status_code=404, detail='Doctor not found')
+    return doctor
--- apps/doctors/services.py
+++ apps/doctors/services.py
@@ -3,34 +3,45 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Session
 from datetime import date
-
+from apps.auth.services import hash_password
 from apps import Doctor
 from .schemas import DoctorCreate
 
 
-async def create_doctor(doctor: DoctorCreate, db: AsyncSession):  # create doctor
-    doctor = Doctor(**doctor.model_dump())
-    db.add(doctor)
+async def create_doctor_service(doctor: DoctorCreate, db: AsyncSession):  # create doctor
+    
+    password = hash_password(doctor.hashed_password)
+    print(password)
+    result = Doctor(
+        first_name=doctor.first_name,
+        last_name=doctor.last_name,
+        phone_number=doctor.phone_number,
+        hashed_password=password,
+        date_of_birth=doctor.date_of_birth,
+        profile_img=doctor.profile_img
+    )
+    db.add(result)
     await db.commit()
-    await db.refresh(doctor)
-    return doctor
+    await db.refresh(result)
+    return result
 
 
-async def get_doctors(db: AsyncSession, skip: int = 0, limit: int = 10):  # list  doctor
+async def get_doctors_service(db: AsyncSession, skip: int = 0, limit: int = 10):  # list  doctor
     result = await db.execute(select(Doctor).offset(skip).limit(limit))
     doctors = result.scalars().all()
     return doctors
 
 
-async def get_doctor(db: AsyncSession, doctor_id: int):  # get 1 doctor
+async def get_doctor_service(db: AsyncSession, doctor_id: int):  # get 1 doctor
     result = await db.execute(select(Doctor).where(Doctor.id == doctor_id))
-    user = result.scalar_one_or_none()
-    return user
+    doctor = result.scalar_one_or_none()
+    return doctor
 
 
-async def delete_doctor(db: AsyncSession, doctor_id: int):  # delete doctor
+async def delete_doctor_service(db: AsyncSession, doctor_id: int):  # delete doctor
     doctor = await db.execute(select(Doctor).where(Doctor.id == doctor_id))
     await db.delete(doctor)
     await db.commit()
     await db.refresh(doctor)
     return doctor
+
